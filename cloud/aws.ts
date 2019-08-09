022977f44b0958ea891566baf42e063d4c795940
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-08-09 23:46:18 +0300

    test pull request deployment

--- cloud/aws.ts
+++ cloud/aws.ts
@@ -1,7 +1,12 @@
 import * as cdk from '@aws-cdk/core'
 import * as api from '@aws-cdk/aws-apigateway'
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as ca from '@aws-cdk/aws-certificatemanager'
+import * as dns from '@aws-cdk/aws-route53'
+import * as target from '@aws-cdk/aws-route53-targets'
+import * as secret from '@aws-cdk/aws-secretsmanager'
 import { _, $ } from './pure'
+import { Duration } from '@aws-cdk/core';
 
 function RestApi(parent: cdk.Construct): api.RestApi {
   const rest = Gateway(parent)
@@ -12,20 +17,37 @@ function RestApi(parent: cdk.Construct): api.RestApi {
   rest.root.addResource('patch').addMethod('PATCH', method)
   rest.root.addResource('post').addMethod('POST', method)
   rest.root.addResource('put').addMethod('PUT', method)
+  
   return rest
 }
 
 function Gateway(parent: cdk.Construct): api.RestApi {
-  const rest = new api.RestApi(parent, 'Gateway',
+  const config = secret.Secret.fromSecretAttributes(parent, 'Secret', {secretArn: 'fog.fish'});
+  const rest = new api.RestApi(parent, 'HttpBinGw',
     {
       deploy: true,
       deployOptions: {
         stageName: 'api'
       },
+      domainName: {
+        certificate: ca.Certificate.fromCertificateArn(parent, 'TLS', config.secretValueFromJson('certificate').toString()),
+        domainName: `${vsn()}.httpbin.fog.fish`
+      },
       failOnWarnings: true,
       endpointTypes: [api.EndpointType.REGIONAL]
     }
   )
+
+  const zone = dns.HostedZone.fromHostedZoneAttributes(parent, 'HostedZone', {
+    hostedZoneId: config.secretValueFromJson('hostedZoneId').toString(),
+    zoneName: 'fog.fish'
+  })
+  new dns.ARecord(parent, 'DomainName', {
+    zone,
+    ttl: Duration.seconds(60),
+    recordName: `${vsn()}.httpbin.fog.fish`,
+    target: {aliasTarget: new target.ApiGateway(rest)}
+  })
 
   return rest
 }
@@ -47,6 +69,10 @@ function HttpBin(stack: cdk.Construct): cdk.Construct {
   return stack
 }
 
+function vsn(): string {
+  return process.env.VSN || 'local'
+}
+
 const app = new cdk.App()
-$(app, HttpBin, `HttpBin-${process.env.BUILD_VERSION || 'a'}`)
+$(app, HttpBin, `HttpBin-${vsn()}`)
 app.synth()
